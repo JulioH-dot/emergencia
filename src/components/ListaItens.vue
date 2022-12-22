e3b338fd94de1a0aa49f164a49983e2bd7a5100d
Julio-Hdot <juliohenrique1909@gmail.com>
2022-12-22 15:19:15 -0300

    implementando lógica de apresentação de componentes do vue-router

--- src/components/ListaItens.vue
+++ src/components/ListaItens.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        Lista Itens {{ tipo }}
-        <br>
-        <item />
+       
+        <item v-for="(itens, index) in items" 
+            :key="index" :dados="itens" 
+        />
     </div>
 </template>
 
 <script>
 import Item from '@/components/Item.vue'
+import { mapState } from 'vuex'
 
 export default {
     name: 'ListaItens',
@@ -16,6 +18,33 @@ export default {
     },
     props: {
         tipo: String
+    },
+    computed:{
+        ...mapState({
+            enfermeiros: state => state.enfermeiros,
+            socorristas: state => state.socorristas,
+            medicos: state => state.medicos,
+            kitDeReanimacao: state => state.equipamentos.kitsDeReanimacao,
+            carro:state => state.equipamentos.carros,
+            telefones:state => state.equipamentos.telefones,
+        }),
+        items(){
+            switch(this.tipo){
+                case 'enfermeiros':
+                    return this.enfermeiros
+                case 'socorristas':
+                    return this.socorristas
+                case 'medicos':
+                    return this.medicos
+                case 'kit-de-reanimacao':
+                    return this.kitDeReanimacao
+                case 'carros':
+                    return this.carro
+                case 'telefones':
+                    return this.telefones               
+            }
+            return []
+        }
     }
 }
 </script>
